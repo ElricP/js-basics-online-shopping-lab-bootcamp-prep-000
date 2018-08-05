8e4cc6ccebd1ac91ad41819aa59de4f01747b4a2
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-05 21:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,5 +66,6 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you.";
   }
-  var result =`
+  var result =`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  setCart([]);
 }
